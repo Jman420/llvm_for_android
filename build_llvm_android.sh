bc0a7c1f522ccf975c149fc6b18d590dd5b0036e
Justin Giannone <jgiannone@mdsol.com>
2019-07-30 15:49:48 -0400

    Abandon Compiling LLVM as Shared Library

--- build_llvm_android.sh
+++ build_llvm_android.sh
@@ -6,7 +6,7 @@ AndroidCmakeExe="$AndroidSdkDir/cmake/3.10.2.4988404/bin/cmake.exe"
 AndroidNinjaExe="$AndroidSdkDir/cmake/3.10.2.4988404/bin/ninja.exe"
 NdkBundle="$AndroidSdkDir/ndk-bundle/"
 ToolchainFile="$NdkBundle/build/cmake/android.toolchain.cmake"
-ArchTargets=( "armeabi-v7a" "arm64-v8a" "x86" "x86_64" )
+ArchTargets=( "armeabi-v7a" ) #"arm64-v8a" "x86" "x86_64" )
 ArchTriples=( "armv7a-linux-androideabi" "aarch64-linux-android" "i686-linux-android" "x86_64-linux-android" )
 LlvmTargets=( "ARM" "AArch64" "X86" "X86" )
 
@@ -68,51 +68,66 @@ do
     echo "Generating Project Files for Architecture : $archTarget ..."
     pushd $projectDir > /dev/null
     $AndroidCmakeExe \
-        -DBUILD_SHARED_LIBS=OFF \
-        -DLLVM_BUILD_LLVM_DYLIB=ON \
-        -DLLVM_BUILD_BENCHMARKS=OFF \
-        -DLLVM_BUILD_EXAMPLES=OFF \
-        -DLLVM_BUILD_RUNTIME=ON \
-        -DLLVM_BUILD_RUNTIMES=OFF \
-        -DLLVM_BUILD_TESTS=OFF \
-        -DLLVM_BUILD_TOOLS=OFF \
-        -DLLVM_BUILD_UTILS=OFF \
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DCMAKE_INSTALL_PREFIX="$buildFullPath" \
+        -DCMAKE_TOOLCHAIN_FILE="$ToolchainFile" \
+        -DCMAKE_MAKE_PROGRAM="$AndroidNinjaExe" \
         \
-        -DLLVM_INCLUDE_BENCHMARKS=OFF \
-        -DLLVM_INCLUDE_DOCS=OFF \
-        -DLLVM_INCLUDE_EXAMPLES=OFF \
-        -DLLVM_INCLUDE_GO_TESTS=OFF \
-        -DLLVM_INCLUDE_RUNTIMES=ON \
-        -DLLVM_INCLUDE_TESTS=OFF \
-        -DLLVM_INCLUDE_TOOLS=OFF \
-        -DLLVM_INCLUDE_UTILS=OFF \
+        -DANDROID_ABI="$archTarget" \
+        -DANDROID_PLATFORM="$AndroidSystemVersion" \
         \
         -DLLVM_DEFAULT_TARGET_TRIPLE="$archTriple" \
-        -DLLVM_TARGET_ARCH="$llvmTarget" \
         -DLLVM_TARGETS_TO_BUILD="$llvmTarget" \
         \
         -DLLVM_TABLEGEN="../../host-build/bin/llvm-tblgen.exe" \
         -DPYTHON_EXECUTABLE="$NdkBundle/prebuilt/windows-x86_64/bin/python2.7.exe" \
         \
-        -DANDROID_NDK="$NdkBundle" \
-        -DANDROID_ABI="$archTarget" \
+        -DLLVM_INCLUDE_BENCHMARKS=OFF \
+        -DLLVM_INCLUDE_DOCS=OFF \
+        -DLLVM_INCLUDE_EXAMPLES=OFF \
+        -DLLVM_INCLUDE_RUNTIMES=OFF \
+        -DLLVM_INCLUDE_TESTS=OFF \
+        -DLLVM_INCLUDE_GO_TESTS=OFF \
+        -DLLVM_INCLUDE_TOOLS=OFF \
+        -DLLVM_INCLUDE_UTILS=OFF \
         \
-        -DCMAKE_BUILD_TYPE="MinSizeRel" \
-        -DCMAKE_INSTALL_PREFIX="$buildFullPath" \
-        -DCMAKE_CROSSCOMPILING="True" \
-        -DCMAKE_SYSTEM_NAME="Android" \
-        -DANDROID_PLATFORM="android-$AndroidSystemVersion" \
-        -DCMAKE_SYSTEM_VERSION="$AndroidSystemVersion" \
-        -DCMAKE_ANDROID_NDK="$NdkBundle" \
-        -DCMAKE_TOOLCHAIN_FILE="$ToolchainFile" \
-        -DCMAKE_MAKE_PROGRAM="$AndroidNinjaExe" \
+        -DLLVM_BUILD_BENCHMARKS=OFF \
+        -DLLVM_BUILD_EXAMPLES=OFF \
+        -DLLVM_BUILD_RUNTIME=OFF \
+        -DLLVM_BUILD_RUNTIMES=OFF \
+        -DLLVM_BUILD_TESTS=OFF \
+        -DLLVM_BUILD_TOOLS=OFF \
+        -DLLVM_BUILD_UTILS=OFF \
+        \
+        -DLLVM_BUILD_LLVM_DYLIB=OFF \
+        \
+        -DLLVM_ENABLE_PIC=False \
+        \
         -G "Ninja" \
+        \
         "../../$SourceRootPath"
+    if [ $? -ne 0 ]; then
+        echo "Project Generation failed for Architecture : $archTarget !"
+        popd > /dev/null
+        exit 1
+    fi
     
     echo "Building LLVM for Architecture : $archTarget ..."
     $AndroidCmakeExe --build . --target install
-    
+    if [ $? -ne 0 ]; then
+        echo "Compilation failed for Architecture : $archTarget !"
+        popd > /dev/null
+        exit 1
+    fi
     popd > /dev/null
+    
+    #echo "Stripping libLLVM for Architecture : $archTarget ..."
+    #strip --strip-unneeded $buildFullPath\lib\libLLVM.so
+    #if [ $? -ne 0 ]; then
+    #    echo "Stripping failed for Architecture : $archTarget !"
+    #    exit 1
+    #fi
+    
     echo "Successfully built LLVM for Architecture : $archTarget !"
 done
 echo "Successfully built LLVM for Android!"
